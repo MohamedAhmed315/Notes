9c978b8e6708e6c7b0c917f4143aa2fd7d36ebc6
mohamed saber <mohamedahmed315@hotmail.com>
2023-03-14 16:28:59 +0200

    notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Notes';
+  @ViewChild('noteInput') noteInput: any;
+  @ViewChild('notesList') notesList: any;
+  lastkey: string = '';
+  counter = 0;
+  notes: string[] = [];
+  keys: string[] = [];
+
+  constructor(private renderer: Renderer2) { }
+
+  addnote(e: any) {
+    e.preventDefault();
+    this.save_note(this.noteInput.nativeElement.value);
+    this.noteInput.nativeElement.value = ' ';
+    this.counter++;
+    this.lastkey = this.lastkey + this.counter
+  }
+
+  save_note(note: string): void {
+    localStorage.setItem(`${this.notes.length}`, note);
+    this.notes.push(note);
+    console.log(this.notes);
+  }
+
+  load_notes(): void {
+    for (const [key, value] of Object.entries(localStorage)) {
+      this.notes.push(`${value}`);
+      this.keys.push(`${key}`);
+      // this.lastkey = `${this.notes.length}`;
+    }
+    // this.lastkey = this.lastkey + this.counter;
+    console.log(this.notes.length);
+  }
+
+  ngOnInit(): void {
+    this.load_notes();
+  }
+
+  clear_all() {
+    localStorage.clear();
+    this.notes.length = 0;
+  }
+
+  delete_note(key: number) {
+    localStorage.removeItem(this.keys[key]);
+    this.notes.splice(key, 1);
+  }
 }
