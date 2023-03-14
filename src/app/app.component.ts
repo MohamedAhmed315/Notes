90ebfc7d0b20e305c8c02f1aff7c575c7de3e01f
mohamed saber <mohamedahmed315@hotmail.com>
2023-03-14 17:11:25 +0200

    notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('noteInput') noteInput: any;
   @ViewChild('notesList') notesList: any;
   lastkey: string = '';
+  viewClearBtn = false;
   counter = 0;
   notes: string[] = [];
   keys: string[] = [];
@@ -27,17 +28,15 @@ export class AppComponent {
   save_note(note: string): void {
     localStorage.setItem(`${this.notes.length}`, note);
     this.notes.push(note);
-    console.log(this.notes);
+    this.view_Clear_btn();
   }
 
   load_notes(): void {
     for (const [key, value] of Object.entries(localStorage)) {
       this.notes.push(`${value}`);
       this.keys.push(`${key}`);
-      // this.lastkey = `${this.notes.length}`;
     }
-    // this.lastkey = this.lastkey + this.counter;
-    console.log(this.notes.length);
+    this.view_Clear_btn();
   }
 
   ngOnInit(): void {
@@ -47,10 +46,21 @@ export class AppComponent {
   clear_all() {
     localStorage.clear();
     this.notes.length = 0;
+    this.view_Clear_btn();
   }
 
   delete_note(key: number) {
     localStorage.removeItem(this.keys[key]);
     this.notes.splice(key, 1);
+    this.view_Clear_btn();
+  }
+
+  view_Clear_btn() {
+    if (this.notes.length != 0) {
+      this.viewClearBtn = true;
+    }
+    else {
+      this.viewClearBtn = false;
+    }
   }
 }
